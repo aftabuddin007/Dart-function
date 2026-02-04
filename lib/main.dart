11e6e0a3cb5d8c52a5f642d5c25f27be30127316
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-02-04 23:59:46 +0600

    add return function

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
-int myFunc(int a, int b) {
-  int c;
-  c = a + b;
-  return c;
-}
+// int myFunc(int a, int b) {
+//   int c;
+//   c = a + b;
+//   return c;
+// }
 
+// void main() {
+
+//   print(myFunc(10, 20));
+//   print(myFunc(12, 20));
+//   print(myFunc(44, 20));
+//   print(myFunc(66, 20));
+//   print(myFunc(61, 20));
+// }
 void main() {
-  var res = myFunc(10, 20);
-  print(res);
+  String greetings() {
+    return "Wellcome";
+  }
+
+  print(greetings());
 }
