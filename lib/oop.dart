2c91399c5c5829020138c6d8df4e1853912ea6ef
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-02-08 23:05:11 +0600

    make hello function

--- lib/oop.dart
+++ lib/oop.dart
@@ -1,15 +1,28 @@
-class Car {
-  String brand;
-  int year;
-  Car(this.brand, this.year);
+// class Car {
+//   String brand;
+//   int year;
+//   Car(this.brand, this.year);
 
-  void showInfo() {
-    print("Brand:$brand , Year:$year");
+//   void showInfo() {
+//     print("Brand:$brand , Year:$year");
+//   }
+// }
+
+// void main() {
+//   Car c1 = Car("BMW", 2010);
+//   c1.showInfo();
+// }
+class Person {
+  String name;
+  Person(this.name);
+
+  void sayHello() {
+    print("Hello, $name");
   }
-}
 
-void main() {
-  Car c1 = Car("BMW", 2010);
-  c1.showInfo();
+ 
 }
-
+ void main() {
+    Person p = Person("Aftab");
+    p.sayHello();
+  }
