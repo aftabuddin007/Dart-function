54dab2690d7c3a516417ab2f7d69aa9eb765d81b
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-02-09 23:30:35 +0600

    practice oop

--- lib/oop.dart
+++ lib/oop.dart
@@ -12,17 +12,33 @@
 //   Car c1 = Car("BMW", 2010);
 //   c1.showInfo();
 // }
-class Person {
-  String name;
-  Person(this.name);
+// class Person {
+//   String name;
+//   Person(this.name);
 
-  void sayHello() {
-    print("Hello, $name");
+//   void sayHello() {
+//     print("Hello, $name");
+//   }
+
+// }
+//  void main() {
+//     Person p = Person("Aftab");
+//     p.sayHello();
+//   }
+class Vehicle {
+  void move() {
+    print("the vehicle is moving");
   }
-
- 
 }
- void main() {
-    Person p = Person("Aftab");
-    p.sayHello();
+
+class Car extends Vehicle {
+  void honk() {
+    print("car honk: beep beep!");
   }
+}
+
+void main() {
+  Car myCar = Car();
+  myCar.honk();
+  myCar.move();
+}
